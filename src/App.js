d79dac2910713a331b9f903dbf2a4e00818f53a7
furher023 <furherv083@gmail.com>
2020-08-16 10:27:03 +0530

    Elevating state to ancestor class for using common data

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import {Navbar, NavbarBrand} from 'reactstrap';
 import Menu from './components/MenuComponent';
 import './App.css';
+import {DISHES} from './shared/Dishes';
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component {
+  
+  constructor(props){
+    super(props);
+    this.state={
+      dishes : DISHES
+    }
+  }
+  
+  render() {
+    return(
+      <div className="App">
       <Navbar dark color="dark">
         <div className="container">
           <NavbarBrand href="/">React</NavbarBrand> 
         </div>
       </Navbar>
-      <Menu />
+      <Menu dishes={this.state.dishes}/>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
